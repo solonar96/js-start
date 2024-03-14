12bb7a2a357db12b67b8c5de87c4dab9b331a18a
Dmytro <solonar96@gmail.com>
2024-03-14 15:39:36 +0200

    added checking script for questions

--- js/script.js
+++ js/script.js
@@ -9,7 +9,27 @@ const numberOfFilms = +prompt("Сколько фильмов вы уже пос�
 		  privat: false
 	  };
 
-personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[prompt("Один из последних просмотренных фильмов?", "")] = prompt("На сколько оцените его?", "");
+
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Один из последних просмотренных фильмов?", ""),
+		b = prompt("На сколько оцените его?", "");
+	if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+}
+
+if (personalMovieDB.count < 10) {
+	alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+	alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+	alert("Вы киноман");
+} else {
+	alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
